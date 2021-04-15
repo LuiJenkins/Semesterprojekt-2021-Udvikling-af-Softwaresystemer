597dc7da0363e6eefcf2d5ef65e8d1853d06694f
rasmus <rasmus@techdan.dk>
2021-04-15 11:49:24 +0200

    Approve + LoginHandler class
    Approve og LoginHandler tilføjet.

---
+++ ST07BSemesterprojekt2021/src/logic/Approved.java
@@ -0,0 +1,46 @@
+package logic;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
+import static java.rmi.server.LogStream.log;
+
+public class Approved<setStatus> {
+    public static void main(String[] args) {
+        int status;
+        int programID;
+        int approvedBy;
+        Date approvedDate;
+    }
+
+    public static void createApproved(int status, int programID, int approvedBy) {
+        //return int
+        throw new UnsupportedOperationException();
+
+    }
+
+    public int getStatus(){
+        //return int
+        throw new UnsupportedOperationException();
+    }
+
+    public void setStatus(){
+        // return this.status
+        throw new UnsupportedOperationException();
+    }
+
+    public int getApprovedBy() {
+        //return int
+        throw new UnsupportedOperationException();
+    }
+
+    public Date getApprovedDate(){
+        // return Date
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ ST07BSemesterprojekt2021/src/logic/LoginHandler.java
@@ -0,0 +1,29 @@
+package logic;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class LoginHandler {
+    public static void main(String[] args) {
+        int userid;
+        String username;
+        String password;
+        int userRole;
+
+
+    }
+
+
+    public static void loginToAccount(String username, String password) {
+       // return string
+        throw new UnsupportedOperationException();
+
+    }
+    public int getUserRole(){
+        //return int
+        throw new UnsupportedOperationException();
+    }
+
+
+
+}
