a65ba8657cb17660db689602dba4041661554b01
Jetof20 <jetof20@student.sdu.dk>
2021-04-15 09:23:55 +0200

    File Structure Rearranged

---
+++ ST07BSemesterprojekt2021/src/presentation/DashboardController.java
@@ -0,0 +1,16 @@
+package presentation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class DashboardController {
+    @FXML
+    public Button logout_button;
+    public Label role_loggedin;
+    public ListView program_list;
+    public ListView program_preview;
+    public TextField newprogram_name;
+    public Button edit_chosen_program;
+    public Button delete_chosen_program;
+
+}
--- ST07BSemesterprojekt2021/src/presentation/Main.java
+++ ST07BSemesterprojekt2021/src/presentation/Main.java
@@ -1,4 +1,4 @@
-package logic;
+package presentation;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
