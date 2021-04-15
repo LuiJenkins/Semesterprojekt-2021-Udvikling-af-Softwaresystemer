60571a9f2d524a124b6175a446b5ec31b2246222
Jetof20 <jetof20@student.sdu.dk>
2021-04-15 11:33:06 +0200

    updated Main

--- ST07BSemesterprojekt2021/src/presentation/Main.java
+++ ST07BSemesterprojekt2021/src/presentation/Main.java
@@ -21,4 +21,50 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public void SwitchScene(String scenePath) {
+
+    }
+    public void makeNewProgram(String programName) {
+        throw new UnsupportedOperationException();
+    }
+    public void editProgram(int programId) {
+        throw new UnsupportedOperationException();
+    }
+    public void acceptProgram(int programId) {
+        throw new UnsupportedOperationException();
+    }
+    public void deleteProgram(int programId) {
+        throw new UnsupportedOperationException();
+    }
+    public void sendProgramToReview(int programId) {
+        throw new UnsupportedOperationException();
+    }
+    public void acceptProgramReview(int programId) {
+        throw new UnsupportedOperationException();
+    }
+    public void denyProgramReview(int programId) {
+        throw new UnsupportedOperationException();
+    }
+    public void createCategory(String categoryName) {
+        throw new UnsupportedOperationException();
+    }
+    public void editCategory(int categoryId) {
+        throw new UnsupportedOperationException();
+    }
+    public void deleteCategory(int categoryId) {
+        throw new UnsupportedOperationException();
+    }
+    public void createPerson(String name,String desc) {
+        throw new UnsupportedOperationException();
+    }
+    public void deletePerson(int personId) {
+        throw new UnsupportedOperationException();
+    }
+    public void updatePerson(int personId,String name,String desc) {
+        throw new UnsupportedOperationException();
+    }
+    public void getCurrentRelevantPrograms() {
+        throw new UnsupportedOperationException();
+    }
 }
