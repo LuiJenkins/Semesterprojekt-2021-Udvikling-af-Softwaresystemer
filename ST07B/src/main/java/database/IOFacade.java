7fcde1672d0168f7cb46c8455782d7f318b6d0b0
Louie <mail@louie.dk>
2021-04-20 13:29:15 +0200

    IO Facade
    lavede IOfacade

--- ST07B/src/main/java/database/IOFacade.java
+++ ST07B/src/main/java/database/IOFacade.java
@@ -1,4 +1,35 @@
 package database;
+import logic.*;
+import logic.Category;
+import logic.Person;
 
 public class IOFacade {
+
+    public void addPerson(Person person){}
+    public void updatePerson(Person person){}
+    public void deletePerson(int ID){}
+    public Person getPerson(int id) {throw new UnsupportedOperationException();}
+    //returns a given person obj with parsed ID
+    public void addCategory(Category category){}
+    public void updateCategory(Category category){}
+    public void deleteCategory(int ID){}
+    public Category getCategory(int ID){throw new UnsupportedOperationException();}
+    public void addProgram(ProgramCredits programCredits ){}
+    public void updateProgram(ProgramCredits programCredits){}
+    public void deleteProgram(int ID){};
+    public ProgramCredits getProgram(int ID){throw new UnsupportedOperationException();}
+    public void addUser(CurrentUser currentUser){}
+    public void updateUser(CurrentUser currentUser){}
+    public void deleteUser(int ID){}
+    public CurrentUser getUser(int ID){throw new UnsupportedOperationException();}
+    public void addTimeslot(TimeSlot timeSlot){}
+    public void updateTimeslot(TimeSlot timeslot){}
+    public void deleteTimeslot(int ID){};
+    public ProgramCredits getTimeslot(int ID){throw new UnsupportedOperationException();}
+    public void addApproved(Approved approved){}
+    public void updateApproved(Approved approved){}
+    public void deleteApproved(int ID){}
+    public Approved getApproved(int ID){throw new UnsupportedOperationException();}
+
+
 }
