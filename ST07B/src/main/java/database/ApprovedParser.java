400f77272744e5066a050976c6883c2fcf77e169
Louie <mail@louie.dk>
2021-04-20 13:55:20 +0200

    database laget

---
+++ ST07B/src/main/java/database/ApprovedParser.java
@@ -0,0 +1,10 @@
+package database;
+
+import logic.Approved;
+
+public class ApprovedParser extends FileParser {
+    public void addApproved(Approved approved){}
+    public void updateApproved(Approved approved){}
+    public void deleteApproved(int ID){}
+    public Approved getApproved(int ID){throw new UnsupportedOperationException();}
+}
