387a9ded229b337150904d9226fd592fb3a7bced
Morten H. Nielsen <morni20@student.sdu.dk>
2021-04-21 10:27:34 +0200

    Update User.java
    User class made with dummy

--- ST07B/src/main/java/logic/User.java
+++ ST07B/src/main/java/logic/User.java
@@ -1,16 +1,29 @@
 package logic;
 
 public class User {
-    private int userID;
+    private int userID=0;
     private String userName;
     private String userPassword;
-    private int userRole;
+    private int userRole = 0;
 
     public User(int userID, String userName, String userPassword, int userRole) {
         this.userID = userID;
         this.userName = userName;
         this.userPassword = userPassword;
         this.userRole = userRole;
+
+        // This is for the first ver.    ##########
+        if (userPassword.contains("1")) {
+            this.userRole=1;
+        } else {
+            if (userPassword.contains("2")) {
+                this.userRole = 2;
+            } else {
+                if (userPassword.contains("3")) {
+                    this.userRole = 3;
+                }
+            }
+        }
     }
 
     public String getUserName() {
@@ -40,4 +53,5 @@ public class User {
     public boolean isAllowed(int level){
         return (this.userRole >= level);
     }
+
 }
